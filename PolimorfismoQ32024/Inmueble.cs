76be1ff5d4a7e2ccce68cef3be301fdba8504890
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-26 20:22:28 -0600

    Herencia mediante sobrecarga

--- PolimorfismoQ32024/Inmueble.cs
+++ PolimorfismoQ32024/Inmueble.cs
@@ -15,5 +15,20 @@ namespace PolimorfismoQ32024
         {
             Console.WriteLine($"{TipoInmueble} con un área de {AreaTerreno}, con ubicación en {Ubicacion} ha sido vendida por el precio de L{Precio}");
         }
+
+        public void Vender(string Cliente)
+        {
+            Console.WriteLine($"{TipoInmueble} con un área de {AreaTerreno}, con ubicación en {Ubicacion} ha sido vendida por el precio de L{Precio} al cliente {Cliente}");
+        }
+
+        public void Vender(double Descuento)
+        {
+            Console.WriteLine($"{TipoInmueble} con un área de {AreaTerreno}, con ubicación en {Ubicacion} ha sido vendida por el precio de L{Precio} con un descuento de L{Descuento}");
+        }
+
+        public void Vender(string Cliente, double Descuento)
+        {
+            Console.WriteLine($"{TipoInmueble} con un área de {AreaTerreno}, con ubicación en {Ubicacion} ha sido vendida por el precio de L{Precio} al cliente {Cliente} y con un descuento de L{Descuento}");
+        }
     }
 }
--- PolimorfismoQ32024/Terreno.cs
+++ PolimorfismoQ32024/Terreno.cs
@@ -4,5 +4,16 @@ namespace PolimorfismoQ32024
     public class Terreno : Inmueble
     {
         public Terreno() { }
+
+        public Terreno(string TipoInmueble)
+        {
+            this.TipoInmueble = TipoInmueble;
+        }
+
+        public Terreno(string TipoInmueble, double Precio)
+        {
+            this.TipoInmueble = TipoInmueble;
+            this.Precio = Precio;
+        }
     }
 }
